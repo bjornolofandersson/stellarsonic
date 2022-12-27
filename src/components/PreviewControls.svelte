d5920062d8ca6124b1583f1c4f5538708b55a335
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-12-27 14:38:47 +0100

    improved blog landing page

---
+++ src/components/PreviewControls.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { MixAudioPlayer } from "../lib/AudioPlayer";
+  import { MusicMix } from "../lib/interfaces";
+  import ProgressBar from "./ProgressBar.svelte";
+
+  export let playlist: MusicMix;
+
+  const player = MixAudioPlayer.getInstance(playlist);
+
+  let isPlaying = false;
+
+  function onClick() {
+    if (player.isPlaying()) {
+      player.pause();
+    } else {
+      player.play()
+    }
+  }
+
+  function update() {
+    isPlaying = player.isPlaying();
+  }
+
+  setInterval(update, 100);
+
+</script>
+
+<div class="w-full">
+  <div class="flex justify-between">
+    <button class="mt-2 px-2" on:click={() => player.skipPrev()}>
+      <span class="material-symbols-outlined">skip_previous</span> 
+    </button>
+
+    {#if isPlaying}
+      <button class="mt-2 px-2" on:click={onClick}>
+        <span class="material-symbols-outlined">pause</span>
+      </button>
+    {:else}
+      <button class="mt-2 px-2" on:click={onClick}>
+        <span class="material-symbols-outlined">play_arrow</span>
+      </button>
+    {/if}
+
+    <button class="mt-2 px-2" on:click={() => player.skipNext()}>
+      <span class="material-symbols-outlined">skip_next</span> 
+    </button>
+  </div>
+
+  <ProgressBar player={player} trackColor="#00000030" progressColor="#d77f7a"/>
+</div>
+
+<style>
+  .material-symbols-outlined {
+    font-size: 18px;
+  }
+</style>
