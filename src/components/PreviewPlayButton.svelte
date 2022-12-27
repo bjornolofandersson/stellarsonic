d17a54fb0e0293654756d02f032c7974076120c0
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-12-27 14:38:47 +0100

    add preview play button

---
+++ src/components/PreviewPlayButton.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { MixAudioPlayer } from "../lib/AudioPlayer";
+  import { MusicMix } from "../lib/interfaces";
+
+  export let playlist: MusicMix;
+
+  const player = MixAudioPlayer.getInstance(playlist);
+
+  let isPlaying = false;
+
+  function onClick() {
+    if (player.isPlaying()) {
+      player.pause();
+    } else {
+      player.play()
+    }
+  }
+
+  function update() {
+    isPlaying = player.isPlaying();
+  }
+
+  setInterval(update, 100);
+
+</script>
+
+<div>
+  <button class="w-16 mr-6 aspect-square text-slate-600" style="border: 1px solid #CCC" on:click={onClick}>
+    <span class="mt-2 material-symbols-outlined">
+      {#if isPlaying}
+        pause
+      {:else}
+        play_arrow
+      {/if}
+    </span>
+  </button>
+</div>
+
+<style>
+  .material-symbols-outlined {
+    font-size: 18px;
+  }
+</style>
