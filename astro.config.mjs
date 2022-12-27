de1c4d1bb9387ebfe9e38b9bc6e6178371810a55
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-12-27 14:38:47 +0100

    initial work on schema suppport

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,13 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
 
+import yaml from '@rollup/plugin-yaml';
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
