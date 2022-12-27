a2f10c96d24fada643d1f6a51921f06fcad6a203
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-12-27 14:38:47 +0100

    major design improvements

--- src/components/Track.svelte
+++ src/components/Track.svelte
@@ -1,10 +1,10 @@
 <script>
-  import * as AudioPlayer from "./AudioPlayer";
+  import * as AudioPlayer from "../lib/AudioPlayer";
 
   export let track;
   export let playlist;
 
-  const {byArtist, name, copyrightYear} = playlist.track[track];
+  const {name, artist, year} = playlist.tracks[track];
   let isPlaying = false;
 
   function onClick() {
@@ -24,16 +24,19 @@
 </script>
 
 <div>
-  <li class="p-4 text-sm">
+  <li class="p-4 text-sm flex" style="border-bottom: 1px solid #FFFFFF08">
     {#if isPlaying}
-      <button class="mt-2 px-2" on:click={onClick}>
+      <button class="mt-2 px-2 opacity-30" on:click={onClick}>
         <span class="material-symbols-outlined">pause</span>
       </button>
     {:else}
-      <button class="mt-2 px-2" on:click={onClick}>
+      <button class="mt-2 px-2 opacity-30" on:click={onClick}>
         <span class="material-symbols-outlined">play_arrow</span>
       </button>
     {/if}
-    <span>{byArtist} - {name} ({copyrightYear})</span>
+    <div>
+      <span class="text-sm">{name}</span><br/>
+      <span class="text-xs opacity-30">{artist} ({year})</span>
+    </div>
   </li>
 </div>
