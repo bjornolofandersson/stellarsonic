fc3fad3b5844095123f5c0088492873cd1ee020c
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-01-03 17:16:14 +0100

    refactored layouts

--- src/components/playlist/PreviewControls.svelte
+++ src/components/playlist/PreviewControls.svelte
@@ -21,21 +21,15 @@
 <div class="w-full">
   <div class="flex justify-between">
     <button class="mt-2 text-slate-600 dark:text-slate-200" on:click={() => player.skipPrev()}>
-      <span class="material-symbols-outlined">skip_previous</span> 
+      <span class="material-symbols-outlined text-md">skip_previous</span> 
     </button>
 
     <div class="text-xs mt-2 opacity-30 dark:text-white">{track.name}</div>
 
     <button class="mt-2 text-slate-600 dark:text-slate-200" on:click={() => player.skipNext()}>
-      <span class="material-symbols-outlined">skip_next</span> 
+      <span class="material-symbols-outlined text-md">skip_next</span> 
     </button>
   </div>
 
   <ProgressBar player={player} trackColor="#00000020" progressColor="#d77f7a"/>
 </div>
-
-<style>
-  .material-symbols-outlined {
-    font-size: 18px;
-  }
-</style>
