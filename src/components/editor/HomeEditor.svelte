c1aae6cc213674e20a26068a825cf19b2d7c3f2c
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-01-12 13:11:06 +0100

    initial work on index page editor

---
+++ src/components/editor/HomeEditor.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Editor from './Editor.svelte';
+
+  function onSave() {}
+</script>
+
+<Editor pageTitle="Home" onSave={onSave}>
+
+  <div slot="preview"><slot/></div>
+</Editor>
