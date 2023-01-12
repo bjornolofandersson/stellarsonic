cdc087511f430c7274d903877d22d201e99c0765
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-01-12 10:54:16 +0100

    intial work on create mix panel

--- src/components/editor/MixListEditor.svelte
+++ src/components/editor/MixListEditor.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+    import { quintOut } from 'svelte/easing';
+    import { fly } from 'svelte/transition';
   import Editor from './Editor.svelte';
+  import EditorPanel from './EditorPanel.svelte';
 
   export let slugs: string[];
+
+  let showCreate = false;
+
+  function onCreate() {
+    console.log('onCreate')
+    showCreate = true
+  }
 
   function onSave() {
   }
@@ -14,19 +24,27 @@
     <li class="px-2 text-stone-500">Mixes</li>
   </ul>
 
-  <div class="px-8 text-lg">
-    <button class="p-2 align-middle"><span class="material-symbols-outlined">add</span> <span class="-mt-2">Create mix</span></button>
-  </div>
+  {#if !showCreate}
+    <div class="absolute w-full" transition:fly={{ x: -512, duration: 500, easing: quintOut }}>
+      <div class="px-8 text-lg">
+        <button on:click={onCreate} class="p-2 align-middle">
+          <span class="material-symbols-outlined">add</span> <span class="-mt-2">Create mix</span>
+        </button>
+      </div>
 
-
-  <ul class="p-8">
-    {#each slugs as slug}
-      <li>
-        <a class="block text-cyan-700 px-4 py-2 w-full border-b border-stone-400 hover:bg-stone-200" 
-          href="/mixes/{slug}"><span class="material-symbols-outlined">edit_note</span> {slug}</a>
-      </li>
-    {/each}
-  </ul>
+      <ul class="p-8">
+        {#each slugs as slug}
+          <li>
+            <a class="block text-cyan-700 px-4 py-2 w-full border-b border-stone-400 hover:bg-stone-200" 
+              href="/mixes/{slug}"><span class="material-symbols-outlined">edit_note</span> {slug}</a>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  {:else}
+    <EditorPanel title="New mix" onBack={() => {showCreate = false}}>
+    </EditorPanel>
+  {/if}
 
   <div slot="preview"><slot/></div>
 </Editor>
