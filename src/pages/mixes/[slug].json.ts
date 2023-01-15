23b2efd1066adc8a3b7f5bb17a6e728659b84bd8
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-01-15 17:21:18 +0100

    refactored server routes

--- src/pages/mixes/[slug].json.ts
+++ src/pages/mixes/[slug].json.ts
@@ -1,55 +1,12 @@
 import type { APIRoute } from 'astro';
-import { getCollection, getEntry } from 'astro:content';
-import * as yaml from 'yaml';
-import * as fs from 'fs';
+import { PostRouter } from '@lib/server';
+
+const router = new PostRouter('mixes');
 
 export async function getStaticPaths() {
-  const docs = await getCollection('mixes');
-  return docs.map(entry => ({
-    params: { slug: entry.slug },
-  }));
+  return router.getStaticPaths();
 }
 
-export function slugToPath(slug: string) {
-  return `/src/content/mixes/${slug}.md`;
-}
-
-export const get: APIRoute = async ({ params }) => {
-  const slug = params.slug;
-  const entry = await getEntry('mixes', `${slug}.md` as any);
-  return {
-    body: JSON.stringify(entry.data)
-  }
-};
-
-export const put: APIRoute = async ({ params, request }) => {
-  const entry = await getEntry('mixes', `${params.slug}.md` as any);
-  const payload = await request.json();
-
-  const result = {...entry.data, ...payload};
-  const frontmatter = yaml.stringify(result);
-  const output = '---\n' + frontmatter + '---\n' + entry.body;
-
-  try {
-    fs.writeFileSync(slugToPath(params.slug), output);
-    // file written successfully
-  } catch (err) {
-    console.error(err);
-  }
-
-  return {
-    body: JSON.stringify(result)
-  }
-};
-
-export const del: APIRoute = async ({ params }) => {
-  const path = slugToPath(params.slug as string);
-
-  await fs.rmSync(path);
-
-  return {
-    body: JSON.stringify({
-      message: "This was a DELETE!"
-    })
-  }
-}
+export const get: APIRoute = async ctx => router.get(ctx);
+export const put: APIRoute = async ctx => router.put(ctx);
+export const del: APIRoute = async ctx => router.del(ctx);
--- src/pages/mixes/index.json.ts
+++ src/pages/mixes/index.json.ts
@@ -1,23 +1,6 @@
 import type { APIRoute } from 'astro';
-import * as yaml from 'yaml';
-import * as fs from 'fs';
-import slugify from 'slugify';
+import { PostCollectionRouter } from '@lib/server';
 
-export const post: APIRoute = async ({ request }) => {
-  const payload = await request.json();
+const router = new PostCollectionRouter('mixes');
 
-  const frontmatter = yaml.stringify(payload);
-  const output = '---\n' + frontmatter + '---\n';
-  const slug = slugify(payload.title, {lower: true});
-
-  try {
-    fs.writeFileSync(`src/content/mixes/${slug}.md`, output);
-    // file written successfully
-  } catch (err) {
-    console.error(err);
-  }
-
-  return {
-    body: JSON.stringify({data: payload, slug})
-  }
-};
+export const post: APIRoute = ctx => router.post(ctx);
