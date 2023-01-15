3c8eb0329e6478d2b2a8f660a01b706a68100eec
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-01-15 14:50:05 +0100

    put post into store before declaring svelte component

--- src/themes/standard/layouts/MusicPlaylist.svelte
+++ src/themes/standard/layouts/MusicPlaylist.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-  import { post as storedPost } from '@lib/MixStore';
+  import { post } from '@lib/MixStore';
   import Post from './Post.svelte';
-
-  export let post: any;
-
-  storedPost.set(post);
 </script>
 
-<Post image={$storedPost.image} colorPrimary={$storedPost.colors[0]}>
+<Post image={$post.image} colorPrimary={$post.colors[0]}>
   <article>
     <slot name="article"/>
   </article>
   <section slot="header-left" class="text-white mt-12 md:px-8 py-8">
-    <h1 class="text-4xl font-title">{$storedPost.title}</h1>
-    <h2 class="text-xl" style={`color: ${$storedPost.colors[1]}`}>{$storedPost.subtitle || ''}</h2>
-    <p class="text-white opacity-40 text-sm mt-8">{$storedPost.description}</p>
+    <h1 class="text-4xl font-title">{$post.title}</h1>
+    <h2 class="text-xl" style={`color: ${$post.colors[1]}`}>{$post.subtitle || ''}</h2>
+    <p class="text-white opacity-40 text-sm mt-8">{$post.description}</p>
   </section>
   <section slot="header-right">
     <div class="md:-mt-40">
