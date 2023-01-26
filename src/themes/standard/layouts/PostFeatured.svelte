b49b87b2dbef3e830eb695608d6cd1199327ec01
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-01-26 22:06:51 +0100

    transition rest of theme to svelte

--- src/themes/standard/layouts/PostFeatured.svelte
+++ src/themes/standard/layouts/PostFeatured.svelte
@@ -1,33 +1,25 @@
----
-export interface Props {
-  collection: string;
+<script lang="ts">
+  export let post: any;
 
-  slug: string;
+  let icon = '';
 
-  image: string;
-}
-
-const { collection, slug, image } = Astro.props;
-
-let icon = '';
-
-if (collection === 'mixes') {
-  icon = 'graphic_eq';
-}
----
+  if (post.collection === 'mixes') {
+    icon = 'graphic_eq';
+  }
+</script>
 
 <div class="container mx-auto">
   <div class="w-full overflow-hidden">
     <div class="grid grid-cols-12 py-12">
       <div class="col-span-12 lg:col-span-6">
-        <img class="w-full h-full object-cover" src={image} alt=""/>
+        <img class="w-full h-full object-cover" src={post.image} alt=""/>
       </div>
       <div class="bg-white col-span-12 lg:col-span-6 z-10 p-20 relative">
         <span class="material-symbols-outlined absolute top-0 right-0 p-8">
           {icon}
         </span>
         <slot />
-        <a class="block mt-12 p-8 font-primary" href={`${collection}/${slug}`} style="border: 1px solid #00000020">Read more</a>
+        <a class="block mt-12 p-8 font-primary" href={`${post.collection}/${post.slug}`} style="border: 1px solid #00000020">Read more</a>
       </div>
     </div>
   </div>
---
+++ src/themes/standard/layouts/index.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<div class="bg-white">
+  <slot name="header"/>
+  <slot name="featured"/>
+
+  {#if $$slots.mixes}
+    <section class="container mx-auto">
+      <div class="text-xl py-20 flex">
+        <span class="material-symbols-outlined mr-4">graphic_eq</span>
+        <span class="block font-primary">Recent mixtapes</span>
+      </div>
+
+      <div class="grid md:grid-cols-1 lg:grid-cols-1 gap-6">
+        <slot name="mixes"/>
+      </div>
+    </section>
+  {/if}
+</div>
