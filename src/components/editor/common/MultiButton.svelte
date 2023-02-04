f50d0bdd9fa26ac11a0953e49cf76be960133e3d
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-02-04 14:46:11 +0100

    editor multi-button

---
+++ src/components/editor/common/MultiButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let options: string[];
+  //export let onClick: (option: string) => void;
+  export let selected: string = options[0];
+</script>
+
+<div class="w-full flex">
+  {#each options as option}
+    <button on:click={() => {selected = option}}
+      class="w-full p-4 first:rounded-l-lg last:rounded-r-lg {selected === option ? 'bg-[#ffffff60]': 'bg-[#00000010] opacity-70'}">
+      <span>{option}</span>
+    </button>
+  {/each}
+</div>
