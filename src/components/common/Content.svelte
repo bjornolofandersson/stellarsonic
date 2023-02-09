819a506dfd2b6c5413b6499b7da84002a598cab1
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-02-09 16:10:22 +0100

    common component for styling content

---
+++ src/components/common/Content.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let style: any;
+</script>
+
+<div class="content" style={`
+  --theme-font-size-h1: ${style.fontSize.h1}rem;
+  --theme-font-size-h2: ${style.fontSize.h2}rem;
+  --theme-font-size-h3: ${style.fontSize.h3}rem;
+  --theme-font-size-p: ${style.fontSize.p}rem;
+  --theme-text-color-h1: ${style.color.h1};
+  --theme-text-color-h2: ${style.color.h2};
+  --theme-text-color-h3: ${style.color.h3};
+  --theme-text-color-p: ${style.color.p};
+`}>
+  <slot/>
+</div>
+
+<style>
+  .content :global(h1) {
+    font-size: var(--theme-font-size-h1);
+    color: var(--theme-text-color-h1);
+  }
+  .content :global(h2) {
+    font-size: var(--theme-font-size-h2);
+    color: var(--theme-text-color-h2);
+  }
+  .content :global(h3) {
+    font-size: var(--theme-font-size-h3);
+    color: var(--theme-text-color-h3);
+  }
+  .content :global(p) {
+    font-size: var(--theme-font-size-p);
+    color: var(--theme-text-color-p);
+  }
+</style>
