2b2e3984874a0c7fd052544001b25ac729493a10
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-02-13 16:13:59 +0100

    use standard components in subpanel

--- src/editor/SelectGroupOption.svelte
+++ src/editor/SelectGroupOption.svelte
@@ -11,14 +11,14 @@
 </script>
 
 {#if $selected === id}
-  <button on:click={() => selectOption(id)} class="p-4 first:rounded-l-md last:rounded-r-md w-full bg-[#ffffff60] shadow-sm flex gap-4">
+  <button on:click={() => selectOption(id)} class="text-sm p-4 first:rounded-l-md last:rounded-r-md w-full bg-[#ffffff60] shadow-sm flex gap-4">
     {#if icon}
       <span class="material-symbols-outlined">{icon}</span>
     {/if}
     <span><slot/></span>
   </button>
 {:else}
-  <button on:click={() => selectOption(id)} class="p-4 first:rounded-l-md last:rounded-r-md w-full bg-[#00000010] text-stone-500 flex gap-4 hover:bg-[#ffffff60] transition">
+  <button on:click={() => selectOption(id)} class="text-sm p-4 first:rounded-l-md last:rounded-r-md w-full bg-[#00000010] text-stone-500 flex gap-4 hover:bg-[#ffffff60] transition">
     {#if icon}
       <span class="material-symbols-outlined">{icon}</span>
     {/if}
--- src/editor/SubPanel.svelte
+++ src/editor/SubPanel.svelte
@@ -3,6 +3,8 @@
   import { quintOut } from "svelte/easing";
   import { getContext } from "svelte";
 	import { PANELS } from './Sidebar.svelte';
+  import BackArrow from "./BackArrow.svelte";
+  import TitleBar from "./TitleBar.svelte";
   
   export let name: string;
 
@@ -13,18 +15,11 @@
 
 {#if $selected === name}
   <div class="absolute w-full px-8" transition:fly={{ x: 512, duration: 500, easing: quintOut }}>
-    <div class="flex mt-8">
-      <button class="text-stone-400 hover:text-stone-700" on:click={() => selectPanel('main')}>
-        <span class="material-symbols-outlined text-4xl">keyboard_backspace</span>
-      </button>
-    </div>
+    <BackArrow onClick={() => selectPanel('main')} />
 
-    <div class="flex justify-between text-xl">
-      <h2 class="mt-8 text-lg font-[500]">{name.toUpperCase()}</h2>
-      <div class="flex">
-        <slot name="actions"/>
-      </div>
-    </div>
+    <TitleBar title={name}>
+      <slot name="actions"/>
+    </TitleBar>
     <slot/>
   </div>
 {/if}
