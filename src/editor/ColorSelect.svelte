a28328428836c53e07ff7bfb8d8f417cf0d2d7bb
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-02-28 19:55:42 +0100

    minor color improvements

--- src/editor/ColorSelect.svelte
+++ src/editor/ColorSelect.svelte
@@ -12,10 +12,13 @@
 <div class="">
   <button on:click={() => {expanded = !expanded}} class="flex justify-between mt-4 w-full p-4 pb-2 bg-transparent border-b border-[#00000020] placeholder-stone-400 rounded focus:outline-none focus:border-stone-700 focus:text-stone-700 dark:text-stone-300 dark:focus:text-stone-100 dark:border-[#ffffff80] dark:focus:border-[#ffffff]">
     <span>{label}</span>
-    <span class="inline-block w-6 h-6 rounded border border-stone-700" style="background-color: {palette[value]};"></span>
+    <div class="flex gap-4">
+      <span class="inline-block w-6 h-6 rounded border border-stone-700" style="background-color: {palette[value]};"></span>
+      <span class="material-symbols-outlined">{expanded ? 'expand_less' : 'expand_more'}</span>
+    </div>
   </button>
   {#if expanded}
-    <div class="flex justify-start gap-4 bg-[#00000010] px-4 py-4" transition:slide={{ duration: 200, easing: quintOut }}>
+    <div class="flex justify-start gap-4 bg-[#00000010] px-12 py-4 -ml-8 -mr-8" transition:slide={{ duration: 200, easing: quintOut }}>
       {#each palette as color, i}
         <button on:click={() => {value = i}} class="inline-block w-6 h-6 rounded border border-stone-700" style="background-color: {color};"></button>
       {/each}
--- src/themes/standard/playlist/PlaylistPost.svelte
+++ src/themes/standard/playlist/PlaylistPost.svelte
@@ -15,10 +15,10 @@
   <article>
     <slot name="article"/>
   </article>
-  <section slot="header-left" class="text-palette-2 mt-12 md:px-8 py-8">
-    <h1 class="text-4xl font-title">{post.title}</h1>
-    <h2 class="text-xl text-palette-2">{post.subtitle || ''}</h2>
-    <p class="text-white opacity-40 text-sm mt-8">{post.description}</p>
+  <section slot="header-left" class="mt-12 md:px-8 py-8">
+    <h1 class="text-h1 text-4xl font-title">{post.title}</h1>
+    <h2 class="text-h2 text-xl">{post.subtitle || ''}</h2>
+    <p class="text-p text-white opacity-40 text-sm mt-8">{post.description}</p>
   </section>
   <section slot="header-right">
     <div class="md:-mt-40">
