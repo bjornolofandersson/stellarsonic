9190ee1f4f1625bd8b0a93fe326d4b2d8f555a14
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-02-28 19:40:58 +0100

    improved color selector

--- src/editor/ColorSelect.svelte
+++ src/editor/ColorSelect.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
+  import { quintOut } from "svelte/easing";
+  import { slide } from "svelte/transition";
+
   export let value: number;
   export let label: string;
   export let palette: string[];
+
+  let expanded = false;
 </script>
 
-<div class="relative">
-  <label class="absolute font-semi-bold mt-4 ml-4 pointer-events-none flex gap-4" for={label}>
-    <span class="inline-block w-6 h-6 rounded border border-stone-700" style="background-color: {palette[value]};"></span>
+<div class="">
+  <button on:click={() => {expanded = !expanded}} class="flex justify-between mt-4 w-full p-4 pb-2 bg-transparent border-b border-[#00000020] placeholder-stone-400 rounded focus:outline-none focus:border-stone-700 focus:text-stone-700 dark:text-stone-300 dark:focus:text-stone-100 dark:border-[#ffffff80] dark:focus:border-[#ffffff]">
     <span>{label}</span>
-  </label>
-  <select id={label} bind:value={value} class="text-right mb-4 w-full p-4 pb-2 bg-transparent border-b border-[#00000020] placeholder-stone-400 rounded focus:outline-none focus:border-stone-700 focus:text-stone-700 dark:text-stone-300 dark:focus:text-stone-100 dark:border-[#ffffff80] dark:focus:border-[#ffffff]">
-    {#each palette as color, i}
-      <option value={i} style="background-color: {color}">
-        {color}
-      </option>
-    {/each}
-  </select>
+    <span class="inline-block w-6 h-6 rounded border border-stone-700" style="background-color: {palette[value]};"></span>
+  </button>
+  {#if expanded}
+    <div class="flex justify-start gap-4 bg-[#00000010] px-4 py-4" transition:slide={{ duration: 200, easing: quintOut }}>
+      {#each palette as color, i}
+        <button on:click={() => {value = i}} class="inline-block w-6 h-6 rounded border border-stone-700" style="background-color: {color};"></button>
+      {/each}
+    </div>
+  {/if}
 </div>
