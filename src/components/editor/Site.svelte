55095da56001593f7ade336dda8cbe0e13824976
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-03-01 17:08:12 +0100

    removed site module

--- src/components/editor/Site.svelte
+++ src/components/editor/Site.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Action, LinkListItem, List, Scrollable, TitleBar } from "src/editor";
+  import { Action, Input, LinkListItem, List, Scrollable, SettingsModal, SettingsPanel, TitleBar } from "src/editor";
   import { quintOut } from "svelte/easing";
   import { slide } from "svelte/transition";
 
@@ -7,6 +7,11 @@
 
   let showAdd: boolean = false;
   let showAddModal: string | undefined = undefined;
+  let showSettings = false;
+
+  async function onSaveSettings() {
+
+  }
 
   async function onDeletePage(page: any) {
     await fetch(`/pages/${page.path}.json`, {
@@ -73,3 +78,29 @@
   <ul class="w-full p-8">
   </ul>
 </Scrollable>
+
+<SettingsModal bind:show={showSettings} onSave={onSaveSettings}>
+  <SettingsPanel name="Site">
+    <ul class="mt-8">
+      <!--
+      <li>
+        <label for="title" class="text-xs opacity-50">Site title</label>
+        <Input type="text" id="title" bind:value={$site.title}/>
+      </li>
+      <li>
+        <label for="description" class="text-xs opacity-50">Description</label>
+        <Input type="text" id="description" bind:value={$site.description}/>
+      </li>
+      <li>
+        <label for="url" class="text-xs opacity-50">URL</label>
+        <Input type="text" id="url" bind:value={$site.url}/>
+      </li>
+  -->
+    </ul>
+  </SettingsPanel>
+</SettingsModal>
+
+<!--
+<CreatePageModal show={showAddModal === 'page'} />
+<CreateMixModal show={showAddModal === 'mix'} />
+-->
