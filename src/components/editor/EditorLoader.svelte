6915a6ace3d449573c0b40630802107d54ed2e42
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-03-02 09:35:51 +0100

    moved remaining editor components into editor folder

---
+++ src/components/editor/EditorLoader.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  const modules = import.meta.glob('../../modules/*/*.editor.svelte', {eager: true});
+
+  export let module: string;
+  export let props: any;
+
+	const Editor = (modules[`../../modules/${module}/${module}.editor.svelte`] as any).default;
+</script>
+
+<svelte:component this={Editor} {...props}>
+  <slot/>
+</svelte:component>
