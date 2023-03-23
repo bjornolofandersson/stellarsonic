61bf36a5e6a2578677a6224f86f6e8b61373d80a
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-03-23 14:38:28 +0100

    screen breakpoints based on dev or prod

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
 	theme: {
+    screens: {
+      'sm': isDev ? '1060px' : '640px',
+      'md': isDev ? '1188px' : '768px',
+      'lg': isDev ? '1444px' : '1024px',
+      'xl': isDev ? '1700px' : '1280px',
+      '2xl': isDev ? '1956px' : '1536px',
+    },
     extend: {
       backgroundColor: {
         primary: 'var(--theme-bg-primary)',
